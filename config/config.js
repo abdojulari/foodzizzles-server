16b1b5e0066d7d6c90e00051c17477953d020645
Abdul Ojulari <abdulkabirojulari@gmail.com>
2024-01-27 08:26:07 -0700

    add .github/workflow directory

--- config/config.js
+++ config/config.js
@@ -4,20 +4,22 @@ require('dotenv').config(
 
 const config = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_NAME,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
         dialect: 'postgres',
         dialectOptions: {
             bigNumberStrings: true,
         },
     },
     test: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_NAME,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
         dialect: 'postgres',
         dialectOptions: {
             bigNumberStrings: true,
