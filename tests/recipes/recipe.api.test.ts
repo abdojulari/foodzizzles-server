e61e736abea9ab8ba90dddc9e1f2c309a85669e4
Abdul Ojulari <abdulkabirojulari@gmail.com>
2024-02-07 12:51:36 -0700

    fix some error in the ci

--- tests/recipes/recipe.api.test.ts
+++ tests/recipes/recipe.api.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import App  from '../../src/app';
+import App from '../../src/app';
 import RecipeController from '../../src/resources/recipes/recipe.controller';
 
 const app = new App(
@@ -8,21 +8,38 @@ const app = new App(
   ],
   Number(process.env.POSTGRES_PORT)
 );
+
 describe('RecipeController Endpoints', () => {
   
-  it('should get all recipes', async () => {
-    const response = await request(app.app).get('/api/recipes');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toBeInstanceOf(Array);
+  it('should get all recipes', (done) => {
+    request(app.app)
+      .get('/api/recipes')
+      .expect(200)
+      .end((err, response) => {
+        if (err) return done(err);
+
+        expect(response.body).toBeInstanceOf(Array);
+        done();
+      });
   }, 30000);
 
-  it('should get a recipe by ID', async () => {
-    const response = await request(app.app).get('/api/recipes/1');
-    expect(response.statusCode).toBe(200);
+  it('should get a recipe by ID', (done) => {
+    request(app.app)
+      .get('/api/recipes/1')
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
   }, 30000);
 
-  it('should delete a recipe by ID', async () => {
-    const response = await request(app.app).delete('/api/recipes/1');
-    expect(response.statusCode).toBe(200);
+  it('should delete a recipe by ID', (done) => {
+    request(app.app)
+      .delete('/api/recipes/1')
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
   }, 30000);
 });
