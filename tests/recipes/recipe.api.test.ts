0f0314eb30f776b15cc080988144534b9f755d41
Abdul Ojulari <abdulkabirojulari@gmail.com>
2024-02-07 12:11:22 -0700

    fix some error in the ci

--- tests/recipes/recipe.api.test.ts
+++ tests/recipes/recipe.api.test.ts
@@ -12,17 +12,20 @@ describe('RecipeController Endpoints', () => {
   
   it('should get all recipes', async () => {
     const response = await request(app.app).get('/api/recipes');
+    console.log(response.body);
     expect(response.status).toBe(200);
     expect(response.body).toBeInstanceOf(Array);
   });
 
   it('should get a recipe by ID', async () => {
     const response = await request(app.app).get('/api/recipes/1');
+    console.log(response.body);
     expect(response.status).toBe(200);
   });
 
   it('should delete a recipe by ID', async () => {
     const response = await request(app.app).delete('/api/recipes/1');
+    console.log(response.body);
     expect(response.status).toBe(200);
   });
 });
